bcbb359e595c954b18d85933faacac0bc0e91925
PabloCode87 <pabloj.vilches.fernandez@gmail.com>
2024-07-26 17:20:40 +0200

    Cambios para los filtros

--- routes/Talentos.js
+++ routes/Talentos.js
@@ -2,31 +2,28 @@ const express = require('express');
 const Talento = require('../models/Talento');
 const router = express.Router();
 
-// Filtros
 router.get('/', async (req, res) => {
-  try {
-    // Obtener filtros desde los parámetros de consulta
-    const { tier, nombre, ranked, clases } = req.query;
-    // Construir un objeto de consulta dinámico
-    let query = {};
+    const { tier, nombre, clases } = req.query;
+    let filter = {};
+
     if (tier) {
-      query.tier = parseInt(tier); // Convertir a número
+        filter.tier = tier;
     }
+
     if (nombre) {
-      query.nombre = new RegExp(nombre, 'i'); // Búsqueda insensible a mayúsculas
+        filter.nombre = { $regex: nombre, $options: 'i' }; // Búsqueda insensible a mayúsculas/minúsculas
     }
-    if (ranked !== undefined) {
-      query.ranked = ranked === 'true'; // Convertir 'true' o 'false' a booleano
+
+    if (clases && clases !== 'All') {
+        filter.clases = { $in: clases.split(',') };
     }
-    if (clases && clases !== 'Todos') {
-      query.clases = { $in: clases.split(',') }; // Filtra por clases que coincidan con cualquier valor
+
+    try {
+        const talentos = await Talento.find(filter);
+        res.json(talentos);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
-    // Obtener talentos con los filtros aplicados
-    const talentos = await Talento.find(query);
-    res.json(talentos);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
 });
 
 module.exports = router;
